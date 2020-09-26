a6463dcb68a523ceab7966a1f2886dfa446dcdf6
Monil Joshi <monil.joshi@qualitia.local>
2020-09-26 13:01:50 +0530

    Refactor Login Page Class | Added locator value in init method and Pass it to find_element method

--- pages/home/LoginPage.py
+++ pages/home/LoginPage.py
@@ -6,15 +6,21 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
 
+    # Locators
+    _login_link = "Login"
+    _email_field = "user_email"
+    _password_field = "user_password"
+    _login_button = "commit"
+
     def login(self, username, password):
-        loginLink = self.driver.find_element(By.LINK_TEXT, "Login")
+        loginLink = self.driver.find_element(By.LINK_TEXT, self._login_link)
         loginLink.click()
 
-        emailField = self.driver.find_element(By.ID, "user_email")
+        emailField = self.driver.find_element(By.ID, self._email_field)
         emailField.send_keys(username)
 
-        passwordField = self.driver.find_element(By.ID, "user_password")
+        passwordField = self.driver.find_element(By.ID, self._password_field)
         passwordField.send_keys(password)
 
-        loginButton = self.driver.find_element(By.NAME, "commit")
+        loginButton = self.driver.find_element(By.NAME, self._login_button)
         loginButton.click()
