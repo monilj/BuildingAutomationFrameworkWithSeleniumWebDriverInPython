bf654637839fbb9ec280005e9d761bd9ea7b802d
Monil Joshi <monil.joshi@qualitia.local>
2020-09-26 12:22:10 +0530

    Convert Test Case To Page Model framework | Created Login Page and added code in it from Login test Class

--- tests/home/login_test.py
+++ tests/home/login_test.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 from webdriver_manager.chrome import ChromeDriverManager
+from pages.home.LoginPage import LoginPage
 
 
 class LoginTests:
@@ -12,22 +13,8 @@ class LoginTests:
         driver.implicitly_wait(3)
         driver.get(baseURL)
 
-        loginLink = driver.find_element(By.LINK_TEXT, "Login")
-        loginLink.click()
-
-        time.sleep(3)
-
-        emailField = driver.find_element(By.ID, "user_email")
-        emailField.send_keys("test@email.com")
-
-        time.sleep(3)
-
-        passwordField = driver.find_element(By.ID, "user_password")
-        passwordField.send_keys("abcabc")
-
-        loginButton = driver.find_element(By.NAME, "commit")
-        time.sleep(10)
-        loginButton.click()
+        lp = LoginPage(driver)
+        lp.login("test@email.com", "abcabc")
 
         userIcon = driver.find_element(By.XPATH, ".//*[@id='navbar']//span[text()='User Settings']")
         if userIcon is not None:
