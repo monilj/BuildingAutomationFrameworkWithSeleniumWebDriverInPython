871e7435a3560aecd960b1db34965077131b190d
Monil Joshi <monil.joshi@qualitia.local>
2020-10-04 14:53:43 +0530

    Building Automation Framework | Added a class call TestStatus in utilities package whihc gives retruns status of executing test at validation point and suite level

--- tests/home/login_test.py
+++ tests/home/login_test.py
@@ -6,6 +6,7 @@ from pages.home.LoginPage import LoginPage
 import unittest
 import pytest
 import unittest
+from utilities.teststatus import TestStatus
 
 
 @pytest.mark.usefixtures("oneTimeSetUp", "setUp")
@@ -13,18 +14,15 @@ class LoginTests(unittest.TestCase):
     @pytest.fixture(autouse=True)
     def classSetup(self, oneTimeSetUp):
         self.lp = LoginPage(self.driver)
+        self.ts = TestStatus(self.driver)
 
-    # Need to verify two verification points
-    # 1 fails, code will not go to the next verification point
-    # If assert fails, it stops current test execution and
-    # moves to the next test method
     @pytest.mark.run(order=2)
     def test_validLogin(self):
         self.lp.login("test@email.com", "abcabc")
         result1 = self.lp.verifyTitle()
-        assert result1 == True
+        self.ts.mark(result1, "Title Verified")
         result2 = self.lp.verifyLoginSuccessful()
-        assert result2 == True
+        self.ts.markFinal("test_validLogin", result2, "Login was successful")
 
     @pytest.mark.run(order=1)
     def test_invalidLogin(self):
